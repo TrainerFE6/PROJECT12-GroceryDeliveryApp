9ce6d7d6dc354d4d3e538c45182770bee549d571
baagas0 <baagas0@gmail.com>
2024-06-17 04:02:30 +0700

    tes

--- backend/src/app.js
+++ backend/src/app.js
@@ -48,4 +48,24 @@ const db = require('./models');
 // Uncomment this line if you want to sync database model
 // db.sequelize.sync()
 
-module.exports = app;
+// module.exports = app;
+
+// const app = require('./app');
+const config = require('./config/config');
+
+console.log('Hello Node-Express-Mysql with Sequelize Boilerplate!!');
+// require('./cronJobs');
+// eslint-disable-next-line import/order
+const http = require('http');
+// socket initialization
+const server = http.createServer(app);
+// eslint-disable-next-line import/order
+const io = require('socket.io')(server, { cors: { origin: '*' } });
+
+global.io = io;
+require('./config/rootSocket')(io);
+
+server.listen(config.port, () => {
+    console.log('SERVER');
+    console.log(`Listening to port ${config.port}`);
+});
